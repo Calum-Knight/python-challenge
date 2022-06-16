04c16adfc6fd447e9bd45bcb7c27a3a7c3a9ecbc
Calum Knight <calum_reiss@hotmail.co.uk>
2022-06-16 22:23:43 +0100

    PyBank Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,18 +9,31 @@ Decrease = 0
 csvpath = os.path.join("Resources" ,"budget_data.csv")
 
 with open(csvpath, encoding='utf') as csvfile:
-
-    for row in csvfile:
+    
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    
+    next(csv_reader)
+    
+    for row in csv_reader:
         months.append(row[0])
-        ProfLoss += row[1]
-        if row[1] > Increase:
-            Increase = row[1]
-        if row[1] < decrease:
-            Decrease = row[1]
+        ProfLoss = ProfLoss + int(row[1])
+        if int(row[1]) > Increase:
+            Increase = int(row[1])
+            Largest = row[0]
+        if int(row[1]) < Decrease:
+            Decrease = int(row[1])
+            Smallest = row[0]
 
-print(f'Total Months: {len(months[])} \n Total: {ProfLoss} \n Average Change: {Average}')
-print(f'Greatest Increase in Profits: {month:amount}')
 
+Average = ProfLoss / len(months)
+
+
+print(f' Total Months: {len(months)} \n Total: {ProfLoss} \n Average Change: {Average}')
+print(f' Greatest Increase in Profits: {Largest} ({Increase})\n Greatest Decrease in Profits: {Smallest} ({Decrease})')
+
+with open("PyBank_Output.txt","w") as text_file:
+    text_file.write(f' Total Months: {len(months)} \n Total: {ProfLoss} \n Average Change: {Average}')
+    text_file.write(f' Greatest Increase in Profits: {Largest} ({Increase})\n Greatest Decrease in Profits: {Smallest} ({Decrease})')
 
 #Total Number of Months in Data
 #Net Total P&L over entire period
